d2033affe958351a750a392659ba5d051a49cfd3
Theodore Anderson <theoanderson3@gmail.com>
2020-11-28 12:56:02 -0800

    clean up

--- src/index.ts
+++ src/index.ts
@@ -65,4 +65,63 @@ const destroy = async <T>(params: IRequestRequired) =>
     requestMethod: RequestMethod.Destroy,
   });
 
+namespace TraTypedFetch {
+  const request = async <T>(params: IRequest): Promise<IResponse<T>> => {
+    const request = transformRequest(params);
+
+    try {
+      const response = await fetch(request.url, request);
+
+      if (!response.ok) {
+        throw new FetchError(
+          response.statusText,
+          `${request.method}-${request.url}`
+        );
+      }
+
+      const json = await response.json();
+
+      const formattedResponse: IResponse<T> = {
+        headers: response.headers,
+        data: json,
+        status: response.status,
+      };
+
+      return Promise.resolve(formattedResponse);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  const get = async <T>(params: IRequestRequiredGet) =>
+    request<T>({
+      ...params,
+      requestMethod: RequestMethod.Get,
+    });
+
+  const post = async <T>(params: IRequestRequired) =>
+    request<T>({
+      ...params,
+      requestMethod: RequestMethod.Post,
+    });
+
+  const patch = async <T>(params: IRequestRequired) =>
+    request<T>({
+      ...params,
+      requestMethod: RequestMethod.Patch,
+    });
+
+  const put = async <T>(params: IRequestRequired) =>
+    request<T>({
+      ...params,
+      requestMethod: RequestMethod.Put,
+    });
+
+  const destroy = async <T>(params: IRequestRequired) =>
+    request<T>({
+      ...params,
+      requestMethod: RequestMethod.Destroy,
+    });
+}
+
 export { get, destroy, patch, post, put };
